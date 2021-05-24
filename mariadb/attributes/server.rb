68c33a4ad939b199ca6277487f991c3b54e95fa9
Yogaraj <yogi.instruzz@gmail.com>
2021-05-24 22:12:01 +0530

    Update server.rb

--- mariadb/attributes/server.rb
+++ mariadb/attributes/server.rb
@@ -1,4 +1,4 @@
-default[:mysql][:server_root_password] = root_pw
+default[:mysql][:server_root_password] = "root_pw"
 default[:mysql][:name]                 = "mariadb"
 default[:mysql][:bin_dir]              = "/usr/bin"
 default[:mysql][:mysqladmin_bin]       = "#{node[:mysql][:bin_dir]}/mysqladmin"
