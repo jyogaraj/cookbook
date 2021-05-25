dad28763e3c02c6abb948b0a8f27515b3c105279
Yogaraj <yogi.instruzz@gmail.com>
2021-05-25 08:05:14 +0530

    Update default.rb

--- weblayer/recipes/default.rb
+++ weblayer/recipes/default.rb
@@ -17,7 +17,7 @@ execute 'clean metadata' do
     command "yum clean metadata"
 end
 
-['libzip','php-cli','php-common','php-json','php','libxslt','php-xml','oniguruma','php-mbstring'].each do |package|
+['libzip','php-cli','php-common','php-json','php','libxslt','php-xml','oniguruma','php-mbstring','php-pdo','php-mysqlnd'].each do |package|
     package "#{package}" do
         retries 3
         retry_delay 5
